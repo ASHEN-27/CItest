a78855d1710019155081b425c73e7ad1e980ff95
Luoquan <strifekid@gmail.com>
2017-01-29 22:52:26 +0000

    first commit

---
+++ src/main/java/ashen/ashenone/App.java
@@ -0,0 +1,13 @@
+package ashen.ashenone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
